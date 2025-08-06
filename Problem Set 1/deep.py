a1688ac5e9d6acdbf109dd8d14d34143b96ee7c7
LaplaceMkhabela <transformmkhabela@gmail.com>
2025-08-06 12:52:28 +0200

    initial commit

---
+++ Problem Set 1/deep.py
@@ -0,0 +1,10 @@
+def deep():
+    answer = input('What is the Answer to the Great Question of Life, the Universe, and Everything?')
+    if (answer == '42') or (answer == 'forty two') or (answer == 'forty-two'):
+        print('Yes')
+    else:
+        print('No')
+
+
+if __name__ == '__main__':
+    deep()
