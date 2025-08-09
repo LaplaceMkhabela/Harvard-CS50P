f3e9d603c4549036fdcae878aeed76a8a5ef8d0f
LaplaceMkhabela <transformmkhabela@gmail.com>
2025-08-09 02:23:23 +0200

    fixed a bug in test3() function

--- Week 2/Problem Set 2/plates.py
+++ Week 2/Problem Set 2/plates.py
@@ -8,22 +8,27 @@ def main():
         print("Invalid")
 
 def test1(s):
-    if s[0].isalpha() and s[1].isalpha():
-        return True
+    #test if the first two characters are alphabets
+    if len(s) >= 2:
+        if s[0].isalpha() and s[1].isalpha():
+            return True
     else:
         return False
     
 def test2(s):
+    #test if the length of the string is between 2 and 6
     if len(s) >= 2 and len(s) <= 6:
         return True
     else:
-        return 
+        return False
     
 def test3(s):
+    #test if numbers don't start with 0 or no alphabets follow ater digits
     suffix = ''
     for char in s:
         if char.isdigit():
             suffix = s[s.find(char):]
+            break
     
     if suffix == '':
         return True
@@ -33,6 +38,7 @@ def test3(s):
         return False
 
 def test4(s):
+    #test if string contains no punctuation
     for char in s:
         if char in string.punctuation:
             return False
